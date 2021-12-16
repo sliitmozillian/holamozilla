3ae7d1292ab6bcae16401e341a717dfea82cf410
Amantha Jayathilake <amanthajayathilake@gmail.com>
2021-12-16 23:18:56 +0530

    d

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1 style={{color:"black"}}>Mozilla</h1>
+      <img src="https://res.cloudinary.com/fitness-glory/image/upload/v1639545102/Background_image_gv9gzy.jpg" style={{height:"500px", width:"500px"}}/>
     </div>
   );
 }
