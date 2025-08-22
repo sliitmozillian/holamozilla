98fcfab8a1e266e64120105ac400f6388d6f508c
Seniru <senirupasan@gmail.com>
2025-08-23 00:56:59 +0530

    feat: contact

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
 import Register from "./pages/register";
 import Layout from "./components/Layout";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="" element={<Layout />}>
           <Route index element={<Index />} />
           <Route path="register" element={<Register />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </BrowserRouter>
