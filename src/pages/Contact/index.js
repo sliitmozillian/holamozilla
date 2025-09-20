0962f04220e00d38ab4d92e2782e9c9e8f7f8201
Seniru <senirupasan@gmail.com>
2025-09-21 02:04:12 +0530

    chore: update contact page styling
    related Improve the design for contact page
    Fixes #3

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,39 +1,65 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./Contact.css";
-
-// credits to this guy: <a href="https://www.flaticon.com/free-stickers/hacker" title="hacker stickers">Hacker stickers created by stickerfolio - Flaticon</a>
+import { faArrowRight, faAt, faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 export default function Contact() {
   return (
     <main className="contact">
-      <section className="contact-information">
-        <img className="hacker-fox" src="hacker-fox.png" />
-        <h2>Contact Us</h2>
-        <p>
-          Email us at{" "}
-          <a href="mailto:infosliitmcss@gmail.com">
-            infosliitmcss@gmail.com
-          </a>
-        </p>
-        <p>
-          For general inquiries, please visit our{" "}
+      <h2>Contact us</h2>
+      <section className="contact-section">
+        <div className="contact-link">
           <a
-            href="https://www.sliitmozilla.org/contact/"
+            href="https://www.sliitmozilla.org//"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Main Contact Page
-          </a>
-        </p>
-        <p>
-          Explore all our links here:{" "}
+            <FontAwesomeIcon icon={faGlobe} />
+            <div>Our website</div></a>
+        </div>
+        <div className="contact-link">
           <a
-            href="https://links.sliitmozilila.org"
+            href="mailto:info@sliitmozilla.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            links.sliitmozilila.org
-          </a>
-        </p>
+            <FontAwesomeIcon icon={faAt} />
+            <div>Contact email</div></a>
+        </div>
+        <div className="contact-link">
+          <a
+            href="https://facebook.com/sliitmozilla"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faFacebook} />
+            <div>Facebook</div></a>
+        </div>
+        <div className="contact-link">
+          <a
+            href="https://instagram.com/sliitmozilla"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faInstagram} />
+            <div>Instagram</div></a>
+        </div>
+        <div className="contact-link">
+          <a
+            href="https://links.sliitmozilla.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faArrowRight} />
+            <div>Explore all our links</div></a>
+        </div>
+        <p>For general inquiries, please visit our <a
+          href="https://www.sliitmozilla.org/contact/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Main Contact Page
+        </a></p>
       </section>
     </main>
   );
